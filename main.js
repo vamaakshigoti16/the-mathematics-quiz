cb13d411ff7d344a09dcc6d2d9351bb04b075465
Vamaakshi Goti <89195459+vamaakshigoti16@users.noreply.github.com>
2023-05-01 13:48:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var score=0;
+
+function updatescore()
+{
+ score=score+1;
+ document.getElementById("score").innerHTML="Score : "+score;
+}
+
+function savescore()
+{
+    localStorage.setItem("score", score);
+}
+
+ function nextpage()
+ {
+    window.location="www.google.com";
+ }
